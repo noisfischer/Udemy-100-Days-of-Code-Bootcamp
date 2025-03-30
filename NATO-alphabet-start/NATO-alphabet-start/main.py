d67b1774434437f2c32188308d2001036a03cb04
noisfischer <noisfischer@gmail.com>
2025-03-30 19:36:34 -0400

    Updated password generator
    Uses JSON functions to update information. New search button that provides details of existing entries. Uses Try, Except, Else and Finally conditions

--- NATO-alphabet-start/NATO-alphabet-start/main.py
+++ NATO-alphabet-start/NATO-alphabet-start/main.py
@@ -2,11 +2,14 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Write a prompt: ").upper()
-letters = [letter for letter in user_input]
-result = []
-for letter in letters:
-    if letter in dict:
-        result.append(dict[letter])
-print(result)
+def generate_phonetic():
+    user_input = input("Write a prompt: ").upper()
+    try:
+        output = [dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output)
+
+generate_phonetic()
